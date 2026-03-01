5000b025c7336a4c7ce4c34d09a538edd91220df
mehran <mehranmohamadi1311@gmail.com>
2026-03-01 12:37:54 +0330

    feat: add tooltip to copy button

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -13,16 +13,20 @@ export default function ActionButtons({
     <article className="flex items-center space-x-2">
       <button
         disabled={disabled}
-        className={`${disabled && 'pointer-events-none'} icons transition-all duration-300 hover:scale-105`}
+        className={`${disabled && 'pointer-events-none'} icons group relative`}
       >
         <HiSpeakerWave />
       </button>
+
       <button
         disabled={disabled}
-        className={`${disabled && 'pointer-events-none'} icons transition-all duration-300 hover:scale-105`}
+        className={`${disabled && 'pointer-events-none'} icons group relative`}
         onClick={handleCopy}
       >
         <FiCopy />
+        <span className="bg-grey-100 before:bg-grey-100 group:flex absolute -top-5 left-1/2 hidden -translate-1/2 items-center justify-center rounded-2xl border-gray-200 px-2 py-px text-[11px] tracking-wide text-white group-hover:block before:absolute before:-bottom-0.5 before:left-1/2 before:size-1.5 before:-translate-x-1/2 before:rotate-45 before:text-white before:content-['']">
+          Copy
+        </span>
       </button>
     </article>
   );
