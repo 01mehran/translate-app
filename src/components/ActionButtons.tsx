51ca25d55d746bdf057c38f4a3f0429916aa2f1b
mehran <mehranmohamadi1311@gmail.com>
2026-03-01 12:41:51 +0330

    feat: add tooltip to speaker button

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -16,6 +16,9 @@ export default function ActionButtons({
         className={`${disabled && 'pointer-events-none'} icons group relative`}
       >
         <HiSpeakerWave />
+        <span className="bg-grey-100 before:bg-grey-100 group:flex absolute -top-5 left-1/2 hidden -translate-1/2 items-center justify-center rounded-2xl border-gray-200 px-2 py-px text-[11px] tracking-wide text-white group-hover:block before:absolute before:-bottom-0.5 before:left-1/2 before:size-1.5 before:-translate-x-1/2 before:rotate-45 before:text-white before:content-['']">
+          speech
+        </span>
       </button>
 
       <button
